d5e51f430083bdef243939fc0e5879a8db74c751
Augustine Love <augustinelovestephens@gmail.com>
2022-08-08 13:28:34 +0000

    Prime numbers

---
+++ prime.py
@@ -0,0 +1,30 @@
+num=int(input("Enter a number: "))
+print(" ")
+print(f"Prime numbers below {num} are")
+def prime_num():
+    count_prime=0
+    sum=0
+    for n in range(2,num):
+            for i in range (2,n):        
+                if n%i==0:
+                    break
+            else:
+                print(n)
+                count_prime+=1
+                sum+=n
+    average=sum/count_prime
+    print(" ")
+    print(f"Prime numbers printed below {num} are",count_prime)
+    print(" ")
+    print("The sum of the prime numbers printed is",sum)
+    print(" ")
+    print("The average of the prime numbers is",average)
+    
+prime_num()
+    
+        
+    
+                
+        
+                
+                
